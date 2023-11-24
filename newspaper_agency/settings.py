a1c6907d028cc0bfad0a9255b9b7cea010786455
D1st3f <dima.purig@gmail.com>
2023-11-24 17:21:44 +0200

    Fixed link to site.

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "newspaperagency-jk9y.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "newspapersagency-w41c.onrender.com"]
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
